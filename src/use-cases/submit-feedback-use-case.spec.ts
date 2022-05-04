37318f48e993f6f961d53f23a2fdc62d3914637d
Anne Bortoli <annebortoli@gmail.com>
2022-05-04 15:31:32 -0300

    :test_tube: testes de submit feedback implementados

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,18 +1,48 @@
 import { SubmitFeedbackUseCase } from './submit-feedback-use-case';
 
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {} },
+  { sendMail: async () => {} }
+);
+
 describe('Submit feedback', () => {
   it('should be able to submit a feedback', async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    );
+    await expect(
+      submitFeedback.execute({
+        type: 'BUG',
+        comment: 'example comment',
+        screenshot: 'data:image/png;base64,812hdgfuy432g5y2g5y2'
+      })
+    ).resolves.not.toThrow();
+  });
 
+  it('should not be able to submit a feedback without a type', async () => {
+    await expect(
+      submitFeedback.execute({
+        type: '',
+        comment: 'example comment',
+        screenshot: 'data:image/png;base64,812hdgfuy432g5y2g5y2'
+      })
+    ).rejects.toThrow();
+  });
+
+  it('should not be able to submit a feedback without a comment', async () => {
+    await expect(
+      submitFeedback.execute({
+        type: 'BUG',
+        comment: '',
+        screenshot: 'data:image/png;base64,812hdgfuy432g5y2g5y2'
+      })
+    ).rejects.toThrow();
+  });
+
+  it('should not be able to submit a feedback with aN invalid screenshot', async () => {
     await expect(
       submitFeedback.execute({
         type: 'BUG',
         comment: 'example comment',
         screenshot: 'test.jpg'
       })
-    ).resolves.not.toThrow();
+    ).rejects.toThrow();
   });
 });
